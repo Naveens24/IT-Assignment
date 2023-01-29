135300464e27fff028ff3132b3c9e54fcf4f5ed5
Naveen Kumar sahu <84307098+Naveens24@users.noreply.github.com>
2023-01-29 16:03:20 +0530

    Create print_string.cpp

---
+++ print_string.cpp
@@ -0,0 +1,18 @@
+//Program to print string one by one without using function
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char arr[6]= {'N', 'A', 'V', 'E', 'E', 'N'};
+
+    for (int i = 0; i < 6; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+
+
+    return 0;
+}
